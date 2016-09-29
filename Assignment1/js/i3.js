53bd94f4ec3198c82dbc7c6a27f7cb0a4c9e4006
jvanrijswijck <j.p.van.rijswijck@student.vu.nl>
2016-09-29 00:28:38 -0400

    tom fucked up bigtime

---
+++ Assignment1/js/i3.js
@@ -0,0 +1,26 @@
+$(function(){
+  $(".increment").click(function(){
+    var count = parseInt($("~ .count", this).text());
+    
+    if($(this).hasClass("up")) {
+      var count = count + 1;
+        if(count>8){
+            window.location = "i4.html";
+        }
+      
+       $("~ .count", this).text(count);
+    } else {
+      var count = count - 1;
+       $("~ .count", this).text(count);
+        if(count<0){
+            window.location = "http://s2.quickmeme.com/img/5c/5ced9b8639743baa448fdbd0153a182327f884e5ea2a2491e06c334a9bda078c.jpg";
+        }
+    }
+    
+    $(this).parent().addClass("bump");
+    
+    setTimeout(function(){
+      $(this).parent().removeClass("bump");    
+    }, 400);
+  });
+});
